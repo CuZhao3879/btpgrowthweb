22e2b1814347d647ffe3cf685418bf64223de272
CuZhao3879 <ngcuzhao012@gmail.com>
2025-10-25 09:11:52 +0800

    update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,8 +12,8 @@ export default function Document() {
           rel="stylesheet"
         />
         
-        {/* Favicon - Replace with your own */}
-        <link rel="icon" href="/favicon.ico" />
+        {/* Favicon - BTP Growth Logo */}
+        <link rel="icon" href="/favicon.png" type="image/png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
